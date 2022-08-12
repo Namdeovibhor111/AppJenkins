218eb849ad5436aded2195eaaf9c07bbaef5cd77
Namdeovibhor111 <lordvib3@gmail.com>
2022-08-12 15:41:24 +0530

    2nd commit

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -9,9 +9,11 @@ namespace HelloApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to jenkins");
+            Console.WriteLine("2nd build");
             Calc calc = new Calc();
             Console.WriteLine($"Sum = {calc.Add(70,80)}");
             Console.WriteLine($"Sum = {calc.Multiply(4, 5)}");
+            Console.WriteLine($"Sum = {calc.Subtract(20, 5)}");
             Console.ReadLine();
         }
     }
